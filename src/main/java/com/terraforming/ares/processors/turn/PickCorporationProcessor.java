67e553017fd475a015c42e81e439e81291443e37
Oleksii Nikitin <Oleksii.Nikitin@pragmaticplay.com>
2022-06-06 01:57:53 +0300

    Pick corporation fix

--- src/main/java/com/terraforming/ares/processors/turn/PickCorporationProcessor.java
+++ src/main/java/com/terraforming/ares/processors/turn/PickCorporationProcessor.java
@@ -27,7 +27,7 @@ public class PickCorporationProcessor implements TurnProcessor<CorporationChoice
         player.getPlayed().addCard(turn.getCorporationCardId());
 
         Card card = carsService.getCard(turn.getCorporationCardId());
-        TurnResponse turnResponse = card.buildProject(
+        return card.buildProject(
                 MarsContext.builder()
                         .game(game)
                         .player(player)
@@ -35,8 +35,6 @@ public class PickCorporationProcessor implements TurnProcessor<CorporationChoice
                         .cardService(carsService)
                         .build()
         );
-        player.setMc(999);
-        return turnResponse;
     }
 
     @Override
