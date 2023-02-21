a2f2dceaa9a536a1a9e24cb8a82c693855acca1b
Oleksii Nikitin <Oleksii.Nikitin@pragmaticplay.com>
2023-02-21 22:54:33 +0100

    Software Streamlining fix

--- src/main/java/com/terraforming/ares/cards/blue/SoftwareStreamlining.java
+++ src/main/java/com/terraforming/ares/cards/blue/SoftwareStreamlining.java
@@ -2,12 +2,14 @@ package com.terraforming.ares.cards.blue;
 
 import com.terraforming.ares.cards.CardMetadata;
 import com.terraforming.ares.cards.green.DiscoveryExpansionBlueCard;
-import com.terraforming.ares.model.CardAction;
-import com.terraforming.ares.model.Tag;
+import com.terraforming.ares.mars.MarsGame;
+import com.terraforming.ares.model.*;
+import com.terraforming.ares.services.UpgradePhaseHelper;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by oleksii.nikitin
@@ -15,7 +17,7 @@ import java.util.List;
  */
 @RequiredArgsConstructor
 @Getter
-public class SoftwareStreamlining implements DiscoveryExpansionBlueCard {
+    public class SoftwareStreamlining implements DiscoveryExpansionBlueCard {
     private final int id;
     private final CardMetadata cardMetadata;
 
@@ -27,6 +29,21 @@ public class SoftwareStreamlining implements DiscoveryExpansionBlueCard {
                 .description("Upgrade a phase card.")
                 .cardAction(CardAction.SOFTWARE_STREAMLINING)
                 .build();
+    }
+
+    @Override
+    public void postProjectBuiltEffect(MarsContext marsContext, Card project, Map<Integer, List<Integer>> input) {
+        List<Integer> cardInput = input.get(InputFlag.PHASE_UPGRADE_CARD.getId());
+
+        final MarsGame game = marsContext.getGame();
+        final Player player = marsContext.getPlayer();
+
+        UpgradePhaseHelper.upgradePhase(marsContext.getCardService(), game, player, cardInput.get(0));
+    }
+
+    @Override
+    public boolean onBuiltEffectApplicableToItself() {
+        return true;
     }
 
     @Override
