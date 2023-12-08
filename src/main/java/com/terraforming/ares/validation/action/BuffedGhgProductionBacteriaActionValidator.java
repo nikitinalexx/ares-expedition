21017ced7dc6a11157c2c00aceb70bcb0269c060
Oleksii.Nikitin <oleksii.nikitin@pragmaticplay.com>
2023-12-08 19:32:41 +0100

    GhgProduction fix

--- src/main/java/com/terraforming/ares/validation/action/BuffedGhgProductionBacteriaActionValidator.java
+++ src/main/java/com/terraforming/ares/validation/action/BuffedGhgProductionBacteriaActionValidator.java
@@ -1,10 +1,14 @@
 package com.terraforming.ares.validation.action;
 
 import com.terraforming.ares.cards.blue.BuffedGhgProductionBacteria;
+import com.terraforming.ares.cards.blue.GhgProductionBacteria;
 import com.terraforming.ares.mars.MarsGame;
+import com.terraforming.ares.model.InputFlag;
 import com.terraforming.ares.model.Player;
+import com.terraforming.ares.services.TerraformingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 import java.util.Map;
@@ -16,7 +20,8 @@ import java.util.Map;
 @Component
 @RequiredArgsConstructor
 public class BuffedGhgProductionBacteriaActionValidator implements ActionValidator<BuffedGhgProductionBacteria> {
-    private final GhgProductionBacteriaActionValidator validator;
+    public static final String ERROR_MESSAGE = "GhgProductionBacteria invalid input, should be either 1 or 2";
+    private final TerraformingService terraformingService;
 
     @Override
     public Class<BuffedGhgProductionBacteria> getType() {
@@ -25,7 +30,28 @@ public class BuffedGhgProductionBacteriaActionValidator implements ActionValidat
 
     @Override
     public String validate(MarsGame game, Player player, Map<Integer, List<Integer>> inputParameters) {
-        return validator.validate(game, player, inputParameters);
-    }
+        if (CollectionUtils.isEmpty(inputParameters)) {
+            return ERROR_MESSAGE;
+        }
 
+        final List<Integer> addDiscardInput = inputParameters.get(InputFlag.ADD_DISCARD_MICROBE.getId());
+        if (CollectionUtils.isEmpty(addDiscardInput)) {
+            return ERROR_MESSAGE;
+        }
+
+        Integer input = addDiscardInput.get(0);
+        if (input != 1 && input != 2) {
+            return ERROR_MESSAGE;
+        }
+
+        if (input == 2 && player.getCardResourcesCount().get(getType()) < 2) {
+            return "Not enough microbes to raise temperature";
+        }
+
+        if (input == 2 && !terraformingService.canIncreaseTemperature(game)) {
+            return "Can not increase temperature anymore";
+        }
+
+        return null;
+    }
 }
